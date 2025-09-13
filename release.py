331da86c2ce40a250bb54af9865aa24b753f1ab4
afmika <afmichael73@gmail.com>
2025-09-14 01:37:50 +0300

    feat: setup release

---
+++ release.py
@@ -0,0 +1,23 @@
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+cargo_toml = Path("Cargo.toml").read_text(encoding="utf-8")
+
+match = re.search(r'version\s*=\s*"(.*?)"', cargo_toml)
+
+version = match.group(1)
+tag = f"v{version}"
+
+print(f"Releasing {tag} ...")
+
+try:
+    # pass
+    subprocess.run(["git", "tag", tag], check=True)
+    subprocess.run(["git", "push", "origin", tag], check=True)
+except subprocess.CalledProcessError:
+    print("Failed to create or push git tag")
+    sys.exit(1)
+
+print(f"{tag} pushed")
